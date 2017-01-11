63a0ead687047e21e40fee13a3c52d328ef163c1
EnstaRomain <romain.baronnier@ensta-bretagne.org>
2017-01-11 16:09:58 +0100

    Ajoue des Générateurs

--- src/simEntity/Carrefour/CarrefoursNames.java
+++ src/simEntity/Carrefour/CarrefoursNames.java
@@ -4,7 +4,14 @@ public enum CarrefoursNames {
 	I1("I1"),
 	I2("I2"),
 	I3("I3"),
-	I4("I4");
+	I4("I4"),
+	P1("P1"),
+	P2("P2"),
+	P3("P3"),
+	P4("P4"),
+	P5("P5"),
+	P6("P6"),
+	P7("P7");
 	
 	private final String name;
 	
