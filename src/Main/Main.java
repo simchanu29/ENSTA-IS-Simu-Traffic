eeff7126854b12e073e12088e6c48e93f5dde979
Camille78 <camille.soulie19@gmail.com>
2017-01-18 20:38:37 +0100

    frequence de pop opérationelle

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 import enstabretagne.base.time.LogicalDateTime;
 import enstabretagne.base.time.LogicalDuration;
@@ -10,6 +11,9 @@ import enstabretagne.base.utility.loggerimpl.SXLSXExcelDataloggerImpl;
 import enstabretagne.base.utility.loggerimpl.SysOutLogger;
 import enstabretagne.simulation.core.ISimulationDateProvider;
 import fr.ensta.lerouxlu.simu.SimEngine;
+import simEntity.Carrefour.Carrefour;
+import simEntity.Carrefour.CarrefourNames;
+import simEntity.Carrefour.Regle.CarrefourRegle;
 import simEntity.Monitor.SimMonitor;
 
 public class Main {
@@ -27,16 +31,41 @@ public class Main {
 		LogicalDateTime begin = LogicalDateTime.Zero;
 
 		
-		SimEngine engine = new SimEngine(1,begin,LogicalDuration.ofHours(30));
+		SimEngine engine = new SimEngine(1,begin,LogicalDuration.ofHours(24));
 		//Initialisation de l'ensemble des loggers
 		Logger.Init((ISimulationDateProvider) engine, loggersNames, true);
 		SimMonitor sm=new SimMonitor(engine, "Monitor");
+		LinkedList<Integer> freqPopVoitureP1=createListeFreq(40,300,20,100,20);
+		LinkedList<Integer> freqPopVoitureP2=createListeFreq(50,200,30,150,30);
+		LinkedList<Integer> freqPopVoitureP3=createListeFreq(30,100,20,300,15);
+		LinkedList<Integer> freqPopVoitureP4=createListeFreq(20,100,30,200,50);
+		LinkedList<Integer> freqPopVoitureP5=createListeFreq(30,400,20,150,20);
+		LinkedList<Integer> freqPopVoitureP6=createListeFreq(20,50,30,100,10);
+		LinkedList<Integer> freqPopVoitureP7=createListeFreq(30,30,10,100,10);
+		
+		Carrefour p1=new Carrefour(engine, CarrefourNames.P1, freqPopVoitureP1);
+		Carrefour p2=new Carrefour(engine, CarrefourNames.P2, freqPopVoitureP2);
+		Carrefour p3=new Carrefour(engine, CarrefourNames.P3, freqPopVoitureP3);
+		Carrefour p4=new Carrefour(engine, CarrefourNames.P4, freqPopVoitureP4);
+		Carrefour p5=new Carrefour(engine, CarrefourNames.P5, freqPopVoitureP5);
+		Carrefour p6=new Carrefour(engine, CarrefourNames.P6, freqPopVoitureP6);
+		Carrefour p7=new Carrefour(engine, CarrefourNames.P7, freqPopVoitureP7);
+		
 		engine.initialize();	
 		engine.resume();
-		while (engine.triggerNextEvent()) {
-			
+		while (engine.triggerNextEvent()) {			
 		}
 		
 		Logger.Terminate();
+	}
+	
+	public static LinkedList<Integer> createListeFreq(int a, int b, int c, int d, int e) {
+		LinkedList<Integer> freqPopVoiture=new LinkedList<Integer>();
+		freqPopVoiture.add(a);
+		freqPopVoiture.add(b);
+		freqPopVoiture.add(c);
+		freqPopVoiture.add(d);
+		freqPopVoiture.add(e);
+		return freqPopVoiture;
 	}
 }
